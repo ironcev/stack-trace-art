9ca975115ac5d0eb5a095c74778e7e292451c2ab
Igor Roncevic <ironcev@hotmail.com>
2014-03-02 17:38:28 +0100

    "Crossed Fingers" stack trace art class added.

---
+++ Source/StackTraceangelo.Art/CrossedFingers.Wrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackTraceangelo.Art
+{
+    public static class CrossedFingers
+    {
+        public static void Paint()
+        {
+            new \u115f\u3164().ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ();
+        }
+    }
+}
